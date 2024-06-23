eb2f2ebc47928534f47c79c0113ee40fbeb7f8cf
AI-Heima <595504667@qq.com>
2024-06-23 19:56:09 +0800

    feat:前台分类组件封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
+import CategoryPanel from '@/pages/index/components/CategoryPanel.vue'
 import type { BannerItem } from '@/types/home'
 import { getHomeBannerAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
@@ -14,11 +15,17 @@ onLoad(() => getHomeBannerData())
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel />
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>

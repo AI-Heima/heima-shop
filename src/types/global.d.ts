1d4927f3eb80d863869fbb52daba1e5eb213b706
AI-Heima <595504667@qq.com>
2024-06-23 21:41:22 +0800

    feat:猜你喜欢类型定义和列表渲染

---
+++ src/types/global.d.ts
@@ -0,0 +1,30 @@
+export type GuessItem = {
+  /** id */
+  id: string
+  /** 商品名称 */
+  name: string
+  /** 商品描述 */
+  desc: string
+  /** 商品价格 */
+  price: number
+  /** 商品图片 */
+  picture: string
+  /** 商品折扣 */
+  discount: number
+  /** 商品已下单数量 */
+  orderNum: number
+}
+
+/** 首页-热门推荐数据类型 */
+export type PageResult<T> = {
+  /** 总条数 */
+  counts: number
+  /** 每页条数 */
+  pageSize: number
+  /** 总页数 */
+  pages: number
+  /** 当前页数 */
+  page: number
+  /** 当前页数据 */
+  items: T[]
+}

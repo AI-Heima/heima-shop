ace8f5338d8a1f92005ecb205b8474a562f3e766
AI-Heima <595504667@qq.com>
2024-06-24 17:43:51 +0800

    feat:热门推荐分页条件

--- src/pages/hot/hot.vue
+++ src/pages/hot/hot.vue
@@ -22,12 +22,17 @@ uni.setNavigationBarTitle({ title: currUrlMap!.title })
 // 推荐封面图
 const bannerPicture = ref('')
 // 推荐选项
-const subTypes = ref<SubTypeItem[]>([])
+const subTypes = ref<(SubTypeItem & { finish?: boolean })[]>([])
 // 高亮的下标
 const activeIndex = ref(0)
 // 获取热门推荐数据
 const getHotRecommendData = async () => {
-  const res = await getHotRecommendAPI(currUrlMap!.url)
+  const res = await getHotRecommendAPI(currUrlMap!.url, {
+    // 技巧：环境变量，开发环境，修改初始页面方便测试分页结束
+    page: import.meta.env.DEV ? 30 : 1,
+    pageSize: 10,
+  })
+  console.log(res)
   bannerPicture.value = res.result.bannerPicture
   subTypes.value = res.result.subTypes
 }
@@ -41,8 +46,16 @@ onLoad(() => {
 const onScrolltolower = async () => {
   // 获取当前选项
   const currentSubType = subTypes.value[activeIndex.value]
-  // 当前页码累加
-  currentSubType.goodsItems.page++
+  // 分页条件
+  if (currentSubType.goodsItems.page < currentSubType.goodsItems.pages) {
+    // 当前页码累加
+    currentSubType.goodsItems.page++
+  } else {
+    // 标记已结束
+    currentSubType.finish = true
+    // 退出并轻提示
+    return uni.showToast({ icon: 'none', title: '没有更多数据了~' })
+  }
   // 调用API传参
   const res = await getHotRecommendAPI(currUrlMap!.url, {
     subType: currentSubType.id,
@@ -98,7 +111,9 @@ const onScrolltolower = async () => {
           </view>
         </navigator>
       </view>
-      <view class="loading-text">正在加载...</view>
+      <view class="loading-text">
+        {{ item.finish ? '没有更多数据了~' : '正在加载...' }}
+      </view>
     </scroll-view>
   </view>
 </template>

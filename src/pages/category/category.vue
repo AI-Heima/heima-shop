7511d4a1b9daf35148a341015682c6438a0e9cb4
AI-Heima <595504667@qq.com>
2024-06-24 19:27:11 +0800

    feat:商品分类-骨架屏优化

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -5,6 +5,7 @@ import { ref, computed } from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
 import type { BannerItem } from '@/types/home'
 import type { CategoryTopItem } from '@/types/category'
+import PageSkeleton from './components/PageSkeleton.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -21,10 +22,12 @@ const getCategoryTopData = async () => {
   categoryList.value = res.result
 }
 
+// 是否数据加载完毕
+const isFinish = ref(false)
 // 页面加载
-onLoad(() => {
-  getBannerData()
-  getCategoryTopData()
+onLoad(async () => {
+  await Promise.all([getBannerData(), getCategoryTopData()])
+  isFinish.value = true
 })
 
 // 提取当前二级分类数据
@@ -34,7 +37,7 @@ const subCategoryList = computed(() => {
 </script>
 
 <template>
-  <view class="viewport">
+  <view class="viewport" v-if="isFinish">
     <!-- 搜索框 -->
     <view class="search">
       <view class="input">
@@ -85,6 +88,7 @@ const subCategoryList = computed(() => {
       </scroll-view>
     </view>
   </view>
+  <PageSkeleton v-else />
 </template>
 
 <style lang="scss">

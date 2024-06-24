a0cbdd4b476c84c119a9bc38ba73412331f6ed8f
AI-Heima <595504667@qq.com>
2024-06-24 15:57:54 +0800

    feat:热门推荐数据获取

---
+++ src/services/hot.ts
@@ -0,0 +1,12 @@
+import type { PageParams } from '@/types/global'
+import { http } from '@/utils/http'
+
+type HotParams = PageParams & { subType?: string }
+// 通用热门推荐类型
+export const getHotRecommendAPI = (url: string, data?: HotParams) => {
+  return http({
+    method: 'GET',
+    url,
+    data,
+  })
+}

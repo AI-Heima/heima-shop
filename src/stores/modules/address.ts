217c3ad5718f111b2b5fe7b618884e53313a84d3
AI-Heima <595504667@qq.com>
2024-06-26 13:56:16 +0800

    feat:填写订单-收货地址

---
+++ src/stores/modules/address.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { AddressItem } from '@/types/address'
+
+// 定义 Store
+export const useAddressStore = defineStore('address', () => {
+  const selectedAddress = ref<AddressItem>()
+
+  const changeSelectedAddress = (val: AddressItem) => {
+    selectedAddress.value = val
+  }
+
+  return {
+    selectedAddress,
+    changeSelectedAddress,
+  }
+})
